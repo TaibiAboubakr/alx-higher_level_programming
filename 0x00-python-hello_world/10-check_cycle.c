2a836ef493303c1498e19165289ed75194fb5a01
aboubakr <c.aboubakr@gmail.com>
2023-09-04 13:41:50 +0100

    Project 0x00-python-hello_world, remove while boucle to optimize code efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,13 +13,14 @@ int check_cycle(listint_t *list)
 
 	if (list == NULL)
 		return (0);
-
+	
 	adress[0] = list;
 	list = list->next;
-
-	for (i = 1; list; list = list->next, i++)
+	i++;
+	while (list)
 	{
 		adress[i] = list;
+		list = list->next;
 		for (i = 0; adress[i]; i++)
 			if (adress[i] == list)
 				return (1);
