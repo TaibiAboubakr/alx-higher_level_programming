a3a37dddd189129a1d6c5ea5e60d32501bee661c
aboubakr <c.aboubakr@gmail.com>
2023-09-05 13:10:44 +0100

    Project 0x01-python-if_else_loops_functions, add /usr/bin/python3 linefiles 10 11 12 100 101 102

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(f"{i}", end=" ")
+            print("{}".format(i), end=" ")
