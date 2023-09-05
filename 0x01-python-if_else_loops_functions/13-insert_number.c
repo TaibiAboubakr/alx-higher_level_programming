839a8db13818b3b42d093fc0fb72b04ce05bd261
aboubakr <c.aboubakr@gmail.com>
2023-09-05 13:17:10 +0100

    Project 0x01-python-if_else_loops_functions, handle case empty list in task 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,12 +12,16 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *tmp = (*head), *new_node;
 
-	if (head == NULL || (*head) == NULL)
+	if (head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = number;
+	if ((*head) == NULL)
+	{ (*head) = new_node;
+	new_node->next = NULL;
+	return (new_node); }
 	if (tmp->n > number)
 	{
 		new_node->next = (*head);
