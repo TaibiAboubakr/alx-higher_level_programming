bb6fa72539e3006a353aead3e4df9eb28297e125
aboubakr <c.aboubakr@gmail.com>
2023-09-07 13:01:20 +0100

    0x02-python-import_modules add condition if number of args == 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
         print("{} arguments.".format(n_args))
     elif n_args == 1:
         print("1 argument:".format(n_args))
+        print("1: {}".format(sys.argv[1]))
+
     else:
         print("{} arguments:".format(n_args))
         for i in range(1, n_args + 1):
