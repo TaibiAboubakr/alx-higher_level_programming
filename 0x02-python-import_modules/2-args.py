cde4ed26656f163cc77acf6439c9566de96bc4dd
aboubakr <c.aboubakr@gmail.com>
2023-09-07 12:09:48 +0100

    0x02-python-import_modules change formatting to .format in file 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-n_args = len(sys.argv) - 1
-if n_args == 0:
-    print("{} arguments.".format(n_args))
-else:
-    print("{} arguments.".format(n_args))
-    for i in range(1, n_args + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    n_args = len(sys.argv) - 1
+    if n_args == 0:
+        print("{} arguments.".format(n_args))
+    else:
+        print("{} arguments.".format(n_args))
+        for i in range(1, n_args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
