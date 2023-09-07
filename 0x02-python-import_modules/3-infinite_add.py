e40eb812c59312e990513f842f64f852b656989f
aboubakr <c.aboubakr@gmail.com>
2023-09-07 12:22:05 +0100

    0x02-python-import_modules change formatting to .format in file 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
-sum = 0
-n_args = len(sys.argv) - 1
-if n_args == 0:
-    print(n_args)
-else:
-    for i in range(1, n_args + 1):
-        sum += int(sys.argv[i])
-    print(f"{sum}")
+if __name__ == "__main__":
+    sum = 0
+    n_args = len(sys.argv) - 1
+    if n_args == 0:
+        print("{}".format(n_args))
+    else:
+        for i in range(1, n_args + 1):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
