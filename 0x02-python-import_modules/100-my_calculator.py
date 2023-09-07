b039a5ae56a9c6286d7c61585459178f45859e04
aboubakr <c.aboubakr@gmail.com>
2023-09-07 12:37:26 +0100

    0x02-python-import_modules change formatting to .format in file 5 100 101 102 and add first lin /usr/bin/python3

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 import sys
 from calculator import *
-if len(sys.argv) != 4:
-    print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
-    exit(1)
-n1 = (sys.argv[1])
-n2 = (sys.argv[3])
-if not n1.isdigit() or not n2.isdigit():
-    print("Error: <a> and <b> must be a number")
-    exit(1)
-n1 = int(n1)
-n2 = int(n2)
-if sys.argv[2] == "+":
-    print("{:d} + {:d} = {:d} ".format(n1, n2, add(n1, n2)))
-elif sys.argv[2] == "-":
-    print("{:d} - {:d} = {:d} ".format(n1, n2, sub(n1, n2)))
-elif sys.argv[2] == "*":
-    print("{:d} * {:d} = {:d} ".format(n1, n2, mul(n1, n2)))
-elif sys.argv[2] == "/":
-    print("{:d} / {:d} = {:d} ".format(n1, n2, div(n1, n2)))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        exit(1)
+    n1 = (sys.argv[1])
+    n2 = (sys.argv[3])
+    if not n1.isdigit() or not n2.isdigit():
+        print("Error: <a> and <b> must be a number")
+        exit(1)
+    n1 = int(n1)
+    n2 = int(n2)
+    if sys.argv[2] == "+":
+        print("{:d} + {:d} = {:d} ".format(n1, n2, add(n1, n2)))
+    elif sys.argv[2] == "-":
+        print("{:d} - {:d} = {:d} ".format(n1, n2, sub(n1, n2)))
+    elif sys.argv[2] == "*":
+        print("{:d} * {:d} = {:d} ".format(n1, n2, mul(n1, n2)))
+    elif sys.argv[2] == "/":
+        print("{:d} / {:d} = {:d} ".format(n1, n2, div(n1, n2)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
