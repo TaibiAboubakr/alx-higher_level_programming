e55c63e336123e9b0b37d33f41a51bdb3414b53c
aboubakr <c.aboubakr@gmail.com>
2023-09-11 16:50:54 +0100

    tasks 13 is palindrome :  function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,9 +14,9 @@ int is_palindrome(listint_t **head)
 
 	if (head == NULL || (*head) == NULL || tmp->next == NULL)
 		return (1);
-	for (nbr_nodes = 0; tmp; tmp = tmp->next,nbr_nodes++)
-			array[nbr_nodes] = tmp->n;
-	
+	for (nbr_nodes = 0; tmp; tmp = tmp->next, nbr_nodes++)
+		array[nbr_nodes] = tmp->n;
+
 	for (i = 0, j = nbr_nodes - 1; i <= j; i++, j--)
 		if (array[i] != array[j])
 			return (0);
