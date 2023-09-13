ba474cae0105d8e1ec9f2e5aed375a52428fc91c
aboubakr <c.aboubakr@gmail.com>
2023-09-13 15:14:06 +0100

    project 0x04-python-more_data_structures task uniq add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_set  = set(my_list)
-    sum_fn = lambda x: x
-    return sum(map(sum_fn, my_set))
-
-
+    my_set = set(my_list)
+    return sum(map(lambda x: x, my_set))
