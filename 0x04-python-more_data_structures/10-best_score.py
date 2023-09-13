4aaa835992269ae0faf1ce45a8e42df5a14353c8
aboubakr <c.aboubakr@gmail.com>
2023-09-13 17:10:53 +0100

    project 0x04-python-more_data_structures task 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    bestScore = None
+    bestStudent = None
     if a_dictionary is None:
         return None
     else:
-        bestScore = 0
         for key in a_dictionary:
             score = a_dictionary.get(key)
-            if score > bestScore:
+            if bestScore is None or score > bestScore:
                 bestScore = score
                 key_bestScore = key
         return (key_bestScore)
-            
