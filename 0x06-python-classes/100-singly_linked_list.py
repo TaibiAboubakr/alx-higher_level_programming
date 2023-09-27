5f1704249a1f6a2e73e72dc82831b751155e0221
aboubakr <c.aboubakr@gmail.com>
2023-09-27 13:31:41 +0100

    Project 0x06-python-classes file : 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -8,14 +8,60 @@ class Node:
         """Initialize a new Node.
             Args:
                 data (int): The data of the node of a singly linked list.
-                next_node (Node): The next_node of the node of a sll.
+                next_node (Node): The next_node of the node of a ssl.
         """
-        if not isinstance(data, int):
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """
+        Retrieve the data of the node of a singly linked list.
+
+        Returns:
+            int: The data of the node of a singly linked list.
+        """
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        """
+        Set the the data part of the node of a singly linked list.
+
+        Args:
+            value (int): The data part of the node of a singly linked list.
+
+        Raises:
+            TypeError: data must be an integer.
+        """
+        if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        self.data = data
-        if next_node is not None and not isinstance(next_node, Node):
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """
+        Retrieve the next_node of a singly linked list.
+
+        Returns:
+            Node: The next node of a singly linked list.
+        """
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        """
+        Set the the next_node of a singly linked list.
+
+        Args:
+            value (Node): The next_node of a singly linked list.
+
+        Raises:
+            TypeError: next_node must be a Node object.
+        """
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
-        self.next_node = next_node
+        self.__next_node = value
 
 
 """ class that defines a singly linked list  """
@@ -27,16 +73,16 @@ class SinglyLinkedList:
     def __init__(self):
         self.head = None
 
-    def sorted_insert(self, data):
+    def sorted_insert(self, value):
         """ insert new Node in the Singly Linked List """
 
-        new_node = Node(data)
-        if self.head is None or data < self.head.data:
+        new_node = Node(value)
+        if self.head is None or value < self.head.data:
             new_node.next_node = self.head
             self.head = new_node
         else:
             curr = self.head
-            while curr.next_node is not None and curr.next_node.data < data:
+            while curr.next_node is not None and curr.next_node.data < value:
                 curr = curr.next_node
             new_node.next_node = curr.next_node
             curr.next_node = new_node
