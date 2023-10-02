b961a330d8267aeadc8e247e825448e93f583939
aboubakr <c.aboubakr@gmail.com>
2023-10-02 11:03:46 +0100

    Project 0x08-python-more_classes file :101-nqueens.py

--- 0x08-python-more_classes/101.nqueens.py
+++ 0x08-python-more_classes/101.nqueens.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ system module  """
 import sys
-n_args = len(sys.argv)
-if n_args != 2:
-    print("Uage: nqueens N")
-    sys.exit(1)
-N = sys.argv[1]
-if not N.isdigit():
-    print("N must be a number")
-    sys.exit(1)
-N = int(N)
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+
+if __name__ == "__main__":
+    n_args = len(sys.argv)
+    if n_args != 2:
+        print("Uage: nqueens N")
+        sys.exit(1)
+    N = sys.argv[1]
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
