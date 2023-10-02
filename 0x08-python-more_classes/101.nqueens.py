51d2add78e200853a55b0bd2165a75457dca94ea
aboubakr <c.aboubakr@gmail.com>
2023-10-02 10:57:53 +0100

    Project 0x08-python-more_classes file :101-nqueens.py

---
+++ 0x08-python-more_classes/101.nqueens.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+n_args = len(sys.argv)
+if n_args != 2:
+    print("Uage: nqueens N")
+    sys.exit(1)
+N = sys.argv[1]
+if not N.isdigit():
+    print("N must be a number")
+    sys.exit(1)
+N = int(N)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
