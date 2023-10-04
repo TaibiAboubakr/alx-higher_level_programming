0636fc214a22e6156da753018b3dd463f23da794
aboubakr <c.aboubakr@gmail.com>
2023-10-04 12:00:50 +0100

    Project 0x07 3-say_my_name.py file

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" say_my_name module contain say_my_name function """
+
+
 def say_my_name(first_name, last_name=""):
+    """ function thats prints : My name is 'first name' 'last name' """
     if not type(first_name) is str:
         raise TypeError("first_name must be a string")
     if not type(last_name) is str:
