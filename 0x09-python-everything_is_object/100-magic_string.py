83523774e0496f5022caef9f9d8ef8f18acdc9e0
aboubakr <c.aboubakr@gmail.com>
2023-10-06 12:21:56 +0100

    0x09-python-everything_is_object task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-""" magic_string module """
-
-
 def magic_string(n):
-    """ magic_string function """
-    s = ""
-    for i in range(1, n + 1):
-        s += "BestSchool" * i
+    s = "BestSchool" * n
     return s
