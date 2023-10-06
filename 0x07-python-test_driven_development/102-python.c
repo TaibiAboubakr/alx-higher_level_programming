6d65f99f812abb3cf4669d65432bca966252cab9
aboubakr <c.aboubakr@gmail.com>
2023-10-06 12:00:04 +0100

    task advanced 102 project 0x07 test driven

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,37 +1,17 @@
 #include <Python.h>
 
 void print_python_string(PyObject *p) {
-    // Check if p is a valid string object
     if (PyUnicode_Check(p)) {
-        // Convert the Python string to a C string (UTF-8 encoding)
-        const char *str = PyUnicode_AsUTF8(p);
+        Py_UNICODE *str = PyUnicode_AsUnicode(p);
+        Py_ssize_t length = PyUnicode_GetLength(p);
+        int compact = PyUnicode_IS_COMPACT_ASCII(p);
 
-        if (str != NULL) {
-            // Print the Python string
-            printf("'%s'\n", str);
-        } else {
-            // Failed to decode the string
-            PyErr_Print();
-        }
+        printf("[.] string object info\n");
+        printf("  type: %s\n", compact ? "compact ascii" : "compact unicode object");
+        printf("  length: %ld\n", length);
+        printf("  value: %ls\n", str);
     } else {
-        // p is not a valid string object
-        PyErr_SetString(PyExc_TypeError, "Expected a string object");
-        PyErr_Print();
+        printf("[.] string object info\n");
+        printf("  [ERROR] Invalid String Object\n");
     }
-}
-
-int main() {
-    // Initialize the Python interpreter
-    Py_Initialize();
-
-    // Create a sample Python string
-    PyObject *sample_string = PyUnicode_DecodeUTF8("Hello, Python!", strlen("Hello, Python!"), "strict");
-
-    // Call the print_python_string function
-    print_python_string(sample_string);
-
-    // Clean up and finalize the Python interpreter
-    Py_Finalize();
-
-    return 0;
 }
