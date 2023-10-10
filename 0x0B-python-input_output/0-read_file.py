3d1ccc42a6e6a53b12f40b61bf5b8dd99a272fb7
aboubakr <c.aboubakr@gmail.com>
2023-10-10 11:09:25 +0100

    0x0B input output and task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" read file module """
+
+
 def read_file(filename=""):
+    """ read file module """
+
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
