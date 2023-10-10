1b4f01f5be612dc164962001101103ec93c1245a
aboubakr <c.aboubakr@gmail.com>
2023-10-10 12:08:30 +0100

    0x0B input output and task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" import json module """
+import json
+""" to_json_string module """
+
+
+def from_json_string(my_str):
+    """ to_json_string function """
+    return (json.dumps(my_str))
