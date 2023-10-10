1285801a58c9384aece8c24892e38ca2616a22bc
aboubakr <c.aboubakr@gmail.com>
2023-10-10 11:57:54 +0100

    0x0B input output and task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+""" to_json_string module """
+
+
+def to_json_string(my_obj):
+    """ to_json_string function """
+    data = json.dumps(my_obj)
+    return (data)
