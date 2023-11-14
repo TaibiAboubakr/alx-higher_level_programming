e61a3d28c98ddfff5d7c9f90aa69b998c1187728
aboubakr <c.aboubakr@gmail.com>
2023-11-14 21:02:02 +0100

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,3 @@
--- Active: 1699973134556@@127.0.0.1@3306
 -- script that lists all records of the table second_table of the database
 -- hbtn_0c_0 in your MySQL server.
 SELECT score, name FROM second_table ORDER BY score DESC;
