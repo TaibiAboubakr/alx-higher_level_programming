536968bef7f0589a9cbde570f30c783faeb820ff
aboubakr <c.aboubakr@gmail.com>
2023-11-15 17:10:59 +0100

    Adding the file 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,6 @@
 use hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+
