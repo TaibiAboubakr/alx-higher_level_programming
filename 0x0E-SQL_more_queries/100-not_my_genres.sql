8736c1257457fd314b6319493023e8c2302a2da4
aboubakr <c.aboubakr@gmail.com>
2023-11-15 18:43:13 +0100

    Updating the file 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -11,7 +11,7 @@ WHERE tv_genres.id NOT IN (
 	FROM tv_genres
 	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	WHERE tv_shows.title != 'Dexter'
+	WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY tv_genres.name ASC;
 
