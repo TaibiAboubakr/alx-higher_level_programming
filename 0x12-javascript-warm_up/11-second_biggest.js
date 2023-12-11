a6bdc184d5c9a3a508b0d0371c20fa9b1a12a0d4
aboubakr <c.aboubakr@gmail.com>
2023-12-11 13:30:45 +0100

    Add the file 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const nArgs = process.argv.length - 1;
+if (nArgs > 1) {
+  let max = parseInt(process.argv[2], 10);
+
+  for (let i = 3; i <= nArgs; i++) {
+    const Num = parseInt(process.argv[i], 10);
+    if (!isNaN(Num) && Num > max) {
+      max = Num;
+    }
+  }
+  let secondMax = parseInt(process.argv[2], 10);
+  for (let i = 3; i <= nArgs; i++) {
+    const Num = parseInt(process.argv[i], 10);
+    if (!isNaN(Num) && Num > secondMax && Num < max) {
+      secondMax = Num;
+    }
+  }
+  console.log(secondMax);
+} else {
+  console.log('0');
+}
