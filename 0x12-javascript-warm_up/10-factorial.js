209ccb57a5443d3e027cffbbc6bb62862123e146
aboubakr <c.aboubakr@gmail.com>
2023-12-11 12:49:45 +0100

    Update the file 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,7 +5,7 @@ if (nArgs > 0 && !isNaN(process.argv[2])) {
   function factorial (n) {
     if (isNaN(n)) {
       return 1;
-    } else if (n <= 1) {
+    } else if (n === 0) {
       return 1;
     } else {
       return n * factorial(n - 1);
