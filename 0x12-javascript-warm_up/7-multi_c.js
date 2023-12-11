c9237d19d6aa52ef72050979cc0a90b8c8b30734
aboubakr <c.aboubakr@gmail.com>
2023-12-11 12:09:17 +0100

    Add the file 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const nArgs = process.argv.length - 2;
-if (nArgs === 0) { console.log('Missing number of occurrences'); } else {
-  if (!isNaN(process.argv[2])) {
+if (nArgs > 0 && !isNaN(process.argv[2])) {
     const x = parseInt(process.argv[2], 10);
     for (let i = 0; i < x; i++) {
       console.log('C is fun');
-    }
+    
+  }
   } else {
     console.log('Missing number of occurrences');
   }
-}
+
