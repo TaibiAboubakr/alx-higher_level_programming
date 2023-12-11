928c23db686ee23720ee9ad4b1dbaf74dedc819f
aboubakr <c.aboubakr@gmail.com>
2023-12-11 11:28:58 +0100

    Add the file 4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const nArgs = process.argv.length - 2;
+const indf = 'is undefined';
+if (nArgs === 0) { console.log('undefined is undefined'); } 
+else if (nArgs === 1) { console.log(process.argv[2] + ' ' + indf); } 
+else { console.log(process.argv[2] + ' ' + 'is' + ' ' +  process.argv[3]); }
