d0ab9ec08d92c1e95462b1ac4c55241bac6395ef
aboubakr <c.aboubakr@gmail.com>
2023-12-11 11:30:24 +0100

    fix semistandard in file 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const nArgs = process.argv.length - 2;
 const indf = 'is undefined';
-if (nArgs === 0) { console.log('undefined is undefined'); } 
-else if (nArgs === 1) { console.log(process.argv[2] + ' ' + indf); } 
-else { console.log(process.argv[2] + ' ' + 'is' + ' ' +  process.argv[3]); }
+if (nArgs === 0) { console.log('undefined is undefined'); } else if (nArgs === 1) { console.log(process.argv[2] + ' ' + indf); } else { console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]); }
