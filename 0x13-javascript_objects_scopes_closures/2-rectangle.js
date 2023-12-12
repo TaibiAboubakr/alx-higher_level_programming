251cf279bcc49ade1563e396505d73bc304350ed
aboubakr <c.aboubakr@gmail.com>
2023-12-12 12:49:30 +0100

    Update the file 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,31 +2,8 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-
-  set width (newWidth) {
-    this.height = this.validateDimension(newWidth);
-  }
-
-  get width () {
-    return this.width;
-  }
-
-  get height () {
-    return this.height;
-  }
-
-  set height (newHeight) {
-    this.height = this.validateDimension(newHeight);
-  }
-
-  validateDimension (value) {
-    if (typeof value === 'number' && value > 0) {
-      return value;
-    } else {
-      return {};
+    if (typeof w === 'number' && typeof w === 'number' && w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
     }
   }
 };
