1a8b2592949c92dc91b102c482e9e111b02fda89
aboubakr <c.aboubakr@gmail.com>
2023-12-12 12:57:00 +0100

    Add the file 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof w === 'number' && w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    let result = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) { result += 'X'; }
+      if (i < this.height - 1) { result += '\n'; }
+    }
+    console.log(result);
+  }
+};
