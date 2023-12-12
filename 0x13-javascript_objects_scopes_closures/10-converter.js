e1a0aaf14464b4eaa407127c4e0906998f6ab8a9
aboubakr <c.aboubakr@gmail.com>
2023-12-12 14:53:50 +0100

    Add the file 10-converter.js

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  function convertToBase (number) {
+    if (number < base) {
+      return number.toString();
+    } else {
+      return convertToBase(Math.floor(number / base)) + (number % base).toString();
+    }
+  }
+  return convertToBase;
+};
