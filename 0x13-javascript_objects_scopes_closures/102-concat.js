8961cfab90be0a14b42fd0120b7827ab5140a87e
aboubakr <c.aboubakr@gmail.com>
2023-12-13 19:28:50 +0100

    Update the file 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,14 +3,13 @@ const fs = require('fs');
 let data = '';
 try {
   const fileAContent = fs.readFileSync(process.argv[2], 'utf-8');
-  console.log(fileAContent);
-  data += fileAContent;
+  data += fileAContent + '\n';
 } catch (error) {
   console.error('Error reading the file:', error.message);
 }
 try {
   const fileBContent = fs.readFileSync(process.argv[3], 'utf-8');
-  data += fileBContent;
+  data += fileBContent + '\n';
 } catch (error) {
   console.error('Error reading the file:', error.message);
 }
@@ -19,11 +18,4 @@ try {
   fs.writeFileSync(process.argv[4], data, 'utf-8');
 } catch (error) {
   console.error('Error writing to the file:', error.message);
-}
-
-try {
-  const fileCContent = fs.readFileSync(process.argv[4], 'utf-8');
-  console.log(fileCContent);
-} catch (error) {
-  console.error('Error reading the file:', error.message);
 }
