65ad6474ec41b754985745ea13bc615b5302e711
Aboubakr <aboubakr@gmail.com>
2023-12-15 15:33:56 +0100

    add 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine, text)
+
+
+def execute_query(username, password, dbname):
+    """ function that execute the query  """
+    DATABASE_URL = f'mysql://{username}:{password}@localhost:3306/{dbname}'
+    engine = create_engine(DATABASE_URL)
+    sql_query = text("select * from states;")
+    with engine.connect() as connection:
+        result = connection.execute(sql_query)
+        empty = True
+        for row in result:
+            print(f"{row[0]}: {row[1]}")
+            empty = False
+            break
+        if empty:
+            print("Nothing")
+
+
+if __name__ == "__main__":
+    username, password, dbname = sys.argv[1:]
+    execute_query(username, password, dbname)
