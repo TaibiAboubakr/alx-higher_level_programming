81666e20bc341f7651ee05972ee9cfc71457cc33
aboubakr <c.aboubakr@gmail.com>
2023-12-17 00:44:09 +0100

    Add the file relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" import modules """
+
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """ State class inherit from Base Class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all,\
+        delete-orphan', backref='states')
