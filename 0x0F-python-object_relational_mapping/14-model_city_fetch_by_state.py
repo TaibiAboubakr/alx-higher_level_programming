9f92b2c13d37def23d28cf36866b0adf87c6a3bb
aboubakr <c.aboubakr@gmail.com>
2023-12-16 15:20:05 +0100

    Add the file 14-model_city_fetch_by_state.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine, text)
+
+
+def execute_query(username, password, dbname):
+    """ function that execute the query  """
+    DATABASE_URL = f'mysql://{username}:{password}@localhost:3306/{dbname}'
+    engine = create_engine(DATABASE_URL)
+    sql_query = text("select cities.id, cities.name, states.name from cities\
+        INNER JOIN states ON (cities.state_id = states.id) ;")
+    with engine.connect() as connection:
+        result = connection.execute(sql_query)
+        for row in result:
+            print(f"{row[2]}: ({row[0]}) {row[1]}")
+
+
+if __name__ == "__main__":
+    username, password, dbname = sys.argv[1:]
+    execute_query(username, password, dbname)
