797a23df56f2401b04f8be73295ea878e1519a45
aboubakr <c.aboubakr@gmail.com>
2023-12-16 14:57:42 +0100

    Add the file 13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine, text)
+from sqlalchemy.orm import sessionmaker
+
+
+def main(username, password, dbname):
+    """ function that execute the query  """
+    DB_URL = f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}'
+    engine = create_engine(DB_URL)
+    session = sessionmaker(bind=engine)()
+    results = session.query(State).filter(State.name.like('%a%')).all()
+    for state in results:
+        session.delete(state)
+        session.commit()
+
+
+if __name__ == "__main__":
+    username, password, dbname = sys.argv[1:]
+    main(username, password, dbname)
