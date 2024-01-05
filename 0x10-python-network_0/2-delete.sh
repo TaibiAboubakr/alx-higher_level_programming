2feb6e40a1de943e21117694c6d1ffb91108a1cc
aboubakr <c.aboubakr@gmail.com>
2024-01-05 23:48:41 +0100

    update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -X DELETE -d "I'm a DELETE request" "$1"
+curl -s -X DELETE "$1"
