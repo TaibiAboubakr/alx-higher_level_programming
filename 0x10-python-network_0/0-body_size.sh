3ebdf0c7026a10c55316a1df25599990c2c45c5b
aboubakr <c.aboubakr@gmail.com>
2024-01-05 23:26:41 +0100

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-# Check if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-response=$(curl -sI "$url" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r\n')
-if [ -z "$response" ]; then
-    echo "Unable to retrieve content size. Check the URL and try again."
-    exit 1
-fi
-
-echo "$response"
+echo $(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r\n')
