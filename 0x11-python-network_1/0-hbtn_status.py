bf81e31dfba78e3350a7476788f2f2d783503ba1
aboubakr <c.aboubakr@gmail.com>
2024-01-07 16:23:49 +0100

    update file hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-# a Python script that fetches https://alx-intranet.hbtn.io/status
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    utf8_content = html.decode('utf-8')
-print("Body response:")
-print(f"\t- type: {type(html)}")
-print(f"\t- content: {html}")
-print(f"\t- utf8 content: {utf8_content}")
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        utf8_content = html.decode('utf-8')
+    print("Body response:")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {utf8_content}")
