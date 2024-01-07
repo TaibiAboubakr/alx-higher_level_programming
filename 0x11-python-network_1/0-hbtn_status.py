cb5b24b8f8e30de29950841cb845fe4c5dc212f6
aboubakr <c.aboubakr@gmail.com>
2024-01-07 17:00:58 +0100

    update files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    
+
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as resp:
         content = resp.read()
