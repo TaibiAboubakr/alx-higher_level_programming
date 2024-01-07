ba4b73f306aedb2db380896f2fdff340a9650edd
aboubakr <c.aboubakr@gmail.com>
2024-01-07 23:13:01 +0100

    update the file 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,12 +9,12 @@ import sys
 if __name__ == "__main__":
     query = "" if len(sys.argv) < 2 else sys.argv[1]
     params = {"q": query}
-    req = requests.get("http://0.0.0.0:5000/search_user", data=params)
+    req = requests.post("http://0.0.0.0:5000/search_user", data=params)
     try:
         data = req.json()
-        if data == {}:
-            print("No result")
-        else:
+        if data:
             print(f"[{data.get('id')}] {data.get('name')}")
+        else:
+            print("No result")
     except ValueError:
         print("Not a valid JSON")
