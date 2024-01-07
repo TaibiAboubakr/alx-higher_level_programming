3ffc0b9593c193876ac7b49bb8faca445c072b80
aboubakr <c.aboubakr@gmail.com>
2024-01-07 23:49:36 +0100

    update the file 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,18 +4,13 @@ and uses the GitHub API to display your id
 """
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     username, password = sys.argv[1:]
-    credentials = (username, password)
-    api_url = f"https://api.github.com/users/{username}"
-    try:
-        resp = requests.get(api_url, auth=credentials)
-        if resp.status_code == 200:
-            data = resp.json()
-            print(data.get('id'))
-        else:
-            print("None")
-    except ValueError:
-        print("Not a valid JSON")
+    credentials = HTTPBasicAuth(username, password)
+    api_url = f"https://api.github.com/user"
+    resp = requests.get(api_url, auth=credentials)
+    data = resp.json()
+    print(data.get('id'))
