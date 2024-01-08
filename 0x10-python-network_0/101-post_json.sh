70e3c96d6d11792408ecd7a699d3b0937384a686
aboubakr <c.aboubakr@gmail.com>
2024-01-08 01:15:56 +0100

    add the file 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -X POST -d @my_json_0 "$1"
+curl -X POST -d @"$2" "$1"
