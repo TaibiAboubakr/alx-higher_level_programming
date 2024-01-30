2122693204f23b4856a74b75e4af1c90328d5692
aboubakr <c.aboubakr@gmail.com>
2024-01-30 16:18:53 +0100

    update the file 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 
 const url = process.argv[2];
 request(url, (error, response, body) => {
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
