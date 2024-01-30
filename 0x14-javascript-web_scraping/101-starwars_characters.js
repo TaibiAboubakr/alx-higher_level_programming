96cd717917c6b10e949c84719bcd581dc5b9309c
aboubakr <c.aboubakr@gmail.com>
2024-01-30 23:43:27 +0100

    add the file 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,24 +1,44 @@
 #!/usr/bin/node
 const request = require('request');
+
 if (process.argv.length === 3) {
   const id = process.argv[2];
   const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
   request(url, (_error, _response, body) => {
     if (body) {
       try {
         const filmData = JSON.parse(body);
         const FilmCharacters = filmData.characters;
-        // console.log(FilmCharacters)
+        const promises = [];
+
         for (let i = 0; i < FilmCharacters.length; i++) {
           const urlpeople = FilmCharacters[i];
-          // console.log(urlpeople)
-          request(urlpeople, (_error, _response, bodyPeople) => {
-          // console.log(body_people)
-            const peopleData = JSON.parse(bodyPeople);
-            console.log(peopleData.name);
-            // console.log(peopleData.name)
+          const characterPromise = new Promise((resolve, reject) => {
+            request(urlpeople, (_error, _response, bodyPeople) => {
+              if (_error) {
+                reject(_error);
+              } else {
+                const peopleData = JSON.parse(bodyPeople);
+                const characterName = peopleData.name;
+                // console.log(characterName);
+                resolve(characterName);
+              }
+            });
           });
+
+          promises.push(characterPromise);
         }
+
+        Promise.all(promises)
+          .then((myList) => {
+            for (const i in myList) {
+              console.log(myList[i]);
+            }
+          })
+          .catch((error) => {
+            console.error('Error fetching character data:', error);
+          });
       } catch (parseError) {
         console.error('Error parsing response body:', parseError);
       }
