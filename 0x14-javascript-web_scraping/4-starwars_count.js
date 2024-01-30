1b8407782665e720d4d39d0ddb024d362bb5044b
aboubakr <c.aboubakr@gmail.com>
2024-01-30 19:53:33 +0100

    update the file 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-if (process.argv.length === 3) {
-  const url = process.argv[2];
-  let count = 0;
-  request(url, (_error, _response, body) => {
-    if (body) {
-      try {
-        const filmsData = JSON.parse(body);
-        for (let i = 0; i < filmsData.results.length; i++) {
-          const film = filmsData.results[i];
-          if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-            count++;
-          }
-        }
-        console.log(count);
-      } catch (parseError) {
-        console.error('Error parsing response body:', parseError);
-      }
-    }
-  });
-} else {
+if (process.argv.length !== 3) {
   console.error('Usage: node script.js <API_URL>');
+  return ;
 }
+const url = process.argv[2];
+let count = 0;
+request(url, (_error, _response, body) => {
+  if (body) {
+    try {
+      const filmsData = JSON.parse(body);
+      for (let i = 0; i < filmsData.results.length; i++) {
+        const film = filmsData.results[i];
+        if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+          count++;
+        }
+      }
+      console.log(count);
+    } catch (parseError) {
+      console.error('Error parsing response body:', parseError);
+    }
+  }
+});
