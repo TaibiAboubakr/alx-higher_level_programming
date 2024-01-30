cc3c964aaf3f96e22483655220e08605f55e6b11
aboubakr <c.aboubakr@gmail.com>
2024-01-30 17:37:44 +0100

    update the file 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,7 +8,12 @@ request(url, (_error, response, body) => {
       const filmsWithWedge = filmsData.results.filter((film) => {
         return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
       });
+      if (filmsWithWedge.length > 0){
       console.log(filmsWithWedge.length);
+      }
+      else {
+      console.log(0);
+      }
     } catch (parseError) {
       console.error('Error parsing response body:', parseError);
     }
