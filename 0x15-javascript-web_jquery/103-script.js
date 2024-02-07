bbfb1215025fedc37fefce71ae091e74adf24f28
aboubakr <c.aboubakr@gmail.com>
2024-02-07 18:28:02 +0100

    update the file 103-script.js

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -2,25 +2,29 @@
 $(document).ready(function () {
   $('INPUT#language_code').on('keydown', function (event) {
     if (event.which === 13) {
-      const language = $('#language_code').val();
-      const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=' + language;
-
-      const fetchHello = async () => {
-        try {
-          const response = await fetch(apiUrl);
-
-          if (!response.ok) {
-            throw new Error(`Failed to fetch data. Status: ${response.status}`);
-          }
-          const data = await response.json();
-          const hello = data.hello;
-          const div = $('DIV#hello');
-          div.html('<p>' + hello + '</p>');
-        } catch (error) {
-          console.error('Error:', error.message);
-        }
-      };
       fetchHello();
     }
   });
+
+  $('INPUT#btn_translate').on('click', function () {
+    fetchHello();
+  });
+
+  const fetchHello = async () => {
+    const language = $('#language_code').val();
+    const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=' + language;
+    try {
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      }
+      const data = await response.json();
+      const hello = data.hello;
+      const div = $('DIV#hello');
+      div.html('<p>' + hello + '</p>');
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
 });
